1c95c36c0b01d6108af5cb597a43ac517475fedd
Nicholas Zentai <nicholasz8687@gmail.com>
2023-05-16 14:12:58 -0400

    completed foundation of about.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'; // import react
-// import { Navigate } from 'react-router-dom'; // import react router
 import Nav from './components/Nav'; // import Nav component
 import About from './components/About'; // import About component
 import Projects from './components/Projects'; // import Projects component
 import Contact from './components/Contact'; // import Contact component
+import Header from './components/Header'; // import Header component
+import Footer from './components/Footer'; // import Footer component
 // import './App.css'; // import App.css
+// import { Navigate } from 'react-router-dom'; // import react router
 
 
 function App () {
   return (
     <main className = "text-gray-400 bg-gray-900 body-font">
+      <Header />
       <Nav />
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 };
