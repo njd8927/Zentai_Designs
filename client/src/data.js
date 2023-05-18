12449707c7b1b414fc03aa139a3d7dae9c1d9bf1
Nicholas Zentai <nicholasz8687@gmail.com>
2023-05-18 17:04:41 -0400

    added links and images to data.js

--- client/src/data.js
+++ client/src/data.js
@@ -1,31 +1,32 @@
 const projects = [
     {
-        title: "E-Commerce Store",
-        subtitle: "MYSQL",
-        description:
-            "Server side application to provide a user with a database for their e-commerce store.",
-        image: "",
-        app_link: "",
-        repo_link: ""
-    },
-    {
-        title: "Bite Buddy",
-        subtitle: "RESTFUL API",
-        description:
-            "Application to allow college students to find the best food deals in their area while also allowing business owners to promote their deals.",
-        image: "",
-        app_link: "",
-        repo_link: ""
-    },
-    {
         title: "devDirect",
         subtitle: "MERN",
         description:
             "Application to allow recent bootcamp grads to upload resumes and additional information to be viewed by potential employers.",
         image: "",
         app_link: "",
-        repo_link: ""
+        repo_link: "https://github.com/Animeet/DevDirect"
     },
+    {
+        title: "Bite Buddy",
+        subtitle: "RESTFUL API",
+        description:
+            "Application to allow college students to find the best food deals in their area while also allowing business owners to promote their deals.",
+        image: "../public/BiteBuddy.png",
+        app_link: "https://intense-anchorage-81492.herokuapp.com/",
+        repo_link: "https://github.com/Animeet/Bite_Buddy"
+    },
+    {
+        title: "E-Commerce Store",
+        subtitle: "MYSQL",
+        description:
+            "Server side application to provide a user with a database for their e-commerce store.",
+        image: "",
+        app_link: "CLI Interface",
+        repo_link: "https://github.com/njd8927/E-Commerce_App"
+    },
+  
 ];
 
 export default projects;
